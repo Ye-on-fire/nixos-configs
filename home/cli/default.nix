253aef7800e800944f9bd504d31fd25909f498fd
Ye-on-fire <y3206771300@outlook.com>
2025-06-26 21:33:22 +0800

    split git

--- home/cli/default.nix
+++ home/cli/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ...}:
 {
   imports = [
-    ./git
+    ./git.nix
   ];
 }
--- home/terminals/kitty.nix
+++ home/terminals/kitty.nix
@@ -4,13 +4,15 @@
     enable = true;
     # set kitty font
     font.name = "Maple Mono NF CN";
-    font.size = 14;
+    font.size = 18;
     # zsh 
     shellIntegration.enableZshIntegration = true;
     # theme
     themeFile = "GruvboxMaterialDarkMedium";
     settings = {
       confirm_os_window_close = 0;
+      cursor_trail = "1";
+      cursor_trail_decay = "0.1 0.4";
     };
   };
 }
