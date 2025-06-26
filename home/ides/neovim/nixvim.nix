245b57524d6de232cc66ce7af59b67af0a4026b6
Ye-on-fire <y3206771300@outlook.com>
2025-06-26 23:19:53 +0800

    some small changes to nxivim

--- home/ides/neovim/nixvim.nix
+++ home/ides/neovim/nixvim.nix
@@ -6,6 +6,15 @@
     clipboard.providers.wl-copy.enable = true;
     plugins = {
       lualine.enable = true;
+      bufferline.enable = true;
+      web-devicons.enable = true;
+    };
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      expandtab = true;
+      clipboard = "unnamedplus";
     };
   };
 }
