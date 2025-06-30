63a3b2c945c831eac85a6552e3efa209b41cc2b6
Ye-on-fire <y3206771300@outlook.com>
2025-06-30 19:05:54 +0800

    reset

--- users/makoto.nix
+++ users/makoto.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
-let isLinux = pkgs.stdenv.isLinux;
-in {
+{
   users.users.makoto = {
     shell = pkgs.zsh;
     isNormalUser = true;
     description = "makoto";
-    extraGroups = if isLinux then [ "networkmanager" "wheel" ] else null;
+    extraGroups = [ "networkmanager" "wheel" ];
   };
 }
